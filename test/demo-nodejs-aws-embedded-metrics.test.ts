3053110e5d41aa93b3d3657aa08f8b63004a324e
intercept6 <4459220+intercept6@users.noreply.github.com>
2022-05-31 12:10:07 +0000

    Initial commit

---
+++ test/demo-nodejs-aws-embedded-metrics.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DemoNodejsAwsEmbeddedMetrics from '../lib/demo-nodejs-aws-embedded-metrics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-nodejs-aws-embedded-metrics-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoNodejsAwsEmbeddedMetrics.DemoNodejsAwsEmbeddedMetricsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
